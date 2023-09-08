07352faeea0ceee1b284b0f28be8b10e767bc831
Noorulhuda Khan <hmnoorulhuda2014@gmail.com>
2023-09-08 23:01:44 +0500

    new commit

---
+++ app.js
@@ -0,0 +1,36 @@
+// CHAP 21-30 PRACTICE & ASSIGNMENT
+
+
+// CHAP21 
+
+// var cityToCheck= prompt("Enter Your City")
+
+// cityToCheck = cityToCheck.toLocaleLowerCase()
+// alert(cityToCheck)
+
+// var cleanestCities=["paris", "new york", "shanghai", "london", "makkah"];
+
+// for ( var i=0; i <=4; i++){
+// if (cityToCheck===cleanestCities[i]){
+
+//     alert("it is among cleanest cities")
+// }
+
+// }
+
+
+
+
+
+// CHAP 22
+
+// var cityToCheck = prompt("enter city")
+// cityToCheck = cityToCheck.toLocaleLowerCase();
+
+// var someChars = cityToCheck.slice(3)
+// console.log(someChars)
+
+
+
+
+
